03c0f5126f43c030bfbbd14f2896ad4caf3eee2e
Jorge Bayuelo <jbayuelo@firsttube.com>
2024-07-22 21:40:35 -0500

    changes on the html index and css, and add a contact html file

--- app.js
+++ app.js
@@ -24,3 +24,13 @@ const navSlide = () => {
 };
 
 navSlide();
+
+// FOR INTERACTIVITY CONTACT FORM //
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#contact form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Form submitted!");
+  });
+});
